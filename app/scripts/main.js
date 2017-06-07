adf3b812a54116ab48a056dfa933afa480003732
Kurtis Young <kcyoung1@ualberta.ca>
2017-06-06 20:57:34 -0700

    create nav bar, start banner

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -76,31 +76,31 @@
 
   // Video Javascript
   // ================
-  var vid = document.getElementById('bgvid');
-  var pauseButton = document.querySelector('#saltInfo button');
-
-  if (window.matchMedia('(prefers-reduced-motion)').matches) {
-    vid.removeAttribute('autoplay');
-    vid.pause();
-    pauseButton.innerHTML = 'Paused';
-  }
-  function vidFade() {
-    vid.classList.add('stopfade');
-  }
-  vid.addEventListener('ended', function() {
-  // only functional if "loop" is removed
-    vid.pause();
-  // to capture IE10
-    vidFade();
-  });
-  pauseButton.addEventListener('click', function() {
-    vid.classList.toggle('stopfade');
-    if (vid.paused) {
-      vid.play();
-      pauseButton.innerHTML = '<i class="material-icons">pause</i>';
-    } else {
-      vid.pause();
-      pauseButton.innerHTML = '&#9654;';
-    }
-  });
+  // var vid = document.getElementById('bgvid');
+  // var pauseButton = document.querySelector('#saltInfo button');
+  //
+  // if (window.matchMedia('(prefers-reduced-motion)').matches) {
+  //   vid.removeAttribute('autoplay');
+  //   vid.pause();
+  //   pauseButton.innerHTML = 'Paused';
+  // }
+  // function vidFade() {
+  //   vid.classList.add('stopfade');
+  // }
+  // vid.addEventListener('ended', function() {
+  // // only functional if "loop" is removed
+  //   vid.pause();
+  // // to capture IE10
+  //   vidFade();
+  // });
+  // pauseButton.addEventListener('click', function() {
+  //   vid.classList.toggle('stopfade');
+  //   if (vid.paused) {
+  //     vid.play();
+  //     pauseButton.innerHTML = '<i class="material-icons">pause</i>';
+  //   } else {
+  //     vid.pause();
+  //     pauseButton.innerHTML = '&#9654;';
+  //   }
+  // });
 })();
